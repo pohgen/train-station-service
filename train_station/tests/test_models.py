682abf71f0e7a31a6109851acb86c51bda059f1c
Alex <delete.rap.stupid@gmail.com>
2024-12-23 17:54:00 +0100

    add property test for Route model

--- train_station/tests/test_models.py
+++ train_station/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from train_station.models import Station
+from train_station.models import Station, Route
 
 
 class StationModelTest(TestCase):
@@ -8,3 +8,11 @@ class StationModelTest(TestCase):
         station = Station(name="Kyiv")
         self.assertEqual(station.longitude, 30.5241361)
         self.assertEqual(station.latitude, 50.4500336)
+
+
+class RouteModelTest(TestCase):
+    def test_distance_property(self):
+        station1 = Station(name="Kyiv")
+        station2 = Station(name="Paris")
+        route = Route(source=station1, destination=station2)
+        self.assertEqual(route.distance, 2031)
