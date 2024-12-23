da6d8e124c6482170960a2aaf432c80e1d06268b
Alex <delete.rap.stupid@gmail.com>
2024-12-23 15:03:35 +0100

    update base urls

--- base/urls.py
+++ base/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/train-station/", include("train_station.urls", namespace="train_station")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
