c0d4149c7ceb60d7ae564ead75eeea647e69d378
Alex <delete.rap.stupid@gmail.com>
2024-12-24 17:10:34 +0100

    add filters to serializer of route

--- train_station/views.py
+++ train_station/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.defaultfilters import title
 from rest_framework import viewsets
 
 from train_station.models import Crew, Station, Route, TrainType, Train, Journey, Order, Ticket
@@ -19,6 +20,27 @@ class StationViewSet(viewsets.ModelViewSet):
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        source = self.request.query_params.get("source")
+        destination =self.request.query_params.get("destination")
+
+        queryset = self.queryset
+
+        if source:
+            source_ids = self._params_to_ints(source)
+            queryset = queryset.filter(source__id__in=source_ids)
+
+        if destination:
+            destination_ids = self._params_to_ints(source)
+            queryset = queryset.filter(destination__id__in=destination_ids)
+
+        return queryset.distinct()
 
 
 class TrainTypeViewSet(viewsets.ModelViewSet):
