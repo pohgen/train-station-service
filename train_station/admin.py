7e4df56bf6bc178a6e72cdea0250b0c76822f439
Alex <delete.rap.stupid@gmail.com>
2024-12-26 02:09:52 +0100

    update admin page

--- train_station/admin.py
+++ train_station/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from train_station.models import (
+    Crew,
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Journey,
+    Order,
+    Ticket,
+)
+
+admin.site.register(Crew)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
+
